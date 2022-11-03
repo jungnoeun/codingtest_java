3d170a4f5f1b732cf4a501d704702e6461fa7755
lovee9523 <noeun1102@gmail,com>
2022-11-03 17:34:12 +0900

    백준 10250번

---
+++ b10250.java
@@ -0,0 +1,83 @@
+package codingtest_java;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class b10250 {
+  public static void main(String[] args) throws IOException {
+    
+    int T;
+    int H, W, N; // Ãþ ¼ö, ¹æ °³¼ö, ¼Õ´Ô ¼ö
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    T = Integer.parseInt(br.readLine());
+    
+  
+    
+    StringTokenizer st;
+    String result = "";
+    int X=0, Y=0;
+    
+    
+    for(int i=0; i<T; i++) {
+     
+      st = new StringTokenizer(br.readLine()," ");
+      H = Integer.parseInt(st.nextToken());
+      W = Integer.parseInt(st.nextToken());
+      N = Integer.parseInt(st.nextToken());
+     
+      // N = 30;
+      // H = 6, W = 7 -> 506È£
+      // 30 / 6 ==0 -> Y = H
+      // X = 30 / 6
+     
+      // N = 31
+      // H = 4, W = 10 -> 308È£
+      // 31 / 4 = 7 + 1 = Y
+      // 31 % H = 3
+      // Y = N/H + 1;
+      // X = N % H;
+      
+      if(N%H == 0) {
+        Y = N/H;
+        X = H;
+      }
+      else {
+        Y = N/H + 1;
+        X = N % H;
+      }
+      
+      
+      
+      if(Y<10){
+        result = String.valueOf(X) +"0"+ String.valueOf(Y);
+      }
+      else {
+        result = String.valueOf(X) + String.valueOf(Y);
+      }
+      
+      
+      
+      bw.write(result + "\n");
+      
+      
+      /**
+       * 2
+          6 12 10
+          30 50 72
+       */
+      
+    }
+    
+    bw.flush();
+    br.close();
+    br.close();
+    
+  }
+}
